7b1e208799da0fb7fc38d12506dc1217a0d60837
Bob Corsaro <bob@embed.ly>
2014-09-02 10:53:03 -0400

    Init

---
+++ showall.py
@@ -0,0 +1,21 @@
+from nbt.nbt import NBTFile
+from os import listdir
+import json
+
+whitelist = json.loads(open('whitelist.json').read())
+playermap = {}
+for o in whitelist:
+    playermap[o['uuid']] = o['name']
+
+
+for f in listdir('./dat/'):
+    p = NBTFile(filename='dat/{}'.format(f))
+    print '*******************************************'
+    print "{} {}".format(playermap.get(f.replace('.dat', ''), 'unknown'), [i.value for i in p['Pos']])
+    print '*******************************************'
+    print 'Inventory >'
+    for i in p['Inventory']:
+        print " * %02d %s" % (i['Count'].value, i['id'])
+    print 'Enderchest >'
+    for i in p['EnderItems']:
+        print " * %02d %s" % (i['Count'].value, i['id'])
