b16a7482dcbb1d91ae2fa5ea11595ca0656f277c
Bob Corsaro <bob@embed.ly>
2014-09-02 11:17:59 -0400

    Adds item names and dimensions

--- showall.py
+++ showall.py
@@ -3,6 +3,12 @@ from nbt.nbt import NBTFile
 from os import listdir
 import json
 
+DIMENSIONS = {
+    0: 'Overworld',
+    1: 'Nether',
+    2: 'End'
+}
+
 whitelist = json.loads(open('whitelist.json').read())
 playermap = {}
 for o in whitelist:
@@ -12,11 +18,19 @@ for o in whitelist:
 for f in listdir('./world/playerdata/'):
     p = NBTFile(filename='world/playerdata/{}'.format(f))
     print '*******************************************'
-    print "{} {}".format(playermap.get(f.replace('.dat', ''), 'unknown'), [i.value for i in p['Pos']])
+    print "{} {} {}".format(playermap.get(f.replace('.dat', ''), 'unknown'), [i.value for i in p['Pos']], DIMENSIONS[p['Dimension'].value])
     print '*******************************************'
     print 'Inventory >'
     for i in p['Inventory']:
-        print " * %02d %s" % (i['Count'].value, i['id'])
+        name = ""
+        try:
+            name = "(%s)" % (i['tag']['display']['Name'])
+        except: pass
+        print " * %02d %s %s" % (i['Count'].value, i['id'], name)
     print 'Enderchest >'
     for i in p['EnderItems']:
-        print " * %02d %s" % (i['Count'].value, i['id'])
+        name = ""
+        try:
+            name = "(%s)" % (i['tag']['display']['Name'])
+        except: pass
+        print " * %02d %s %s" % (i['Count'].value, i['id'], name)
